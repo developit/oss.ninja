ca8da9f9faca0ce78b560712bffee59a3f1a1a62
Jason Miller <jason@developit.ca>
2018-01-11 11:28:27 -0500

    temporary fix for GA

--- src/components/app.js
+++ src/components/app.js
@@ -1,7 +1,6 @@
 import { h, Component } from 'preact';
 import { Router } from 'preact-router';
 import Provider from 'preact-context-provider';
-// import GAnalytics from 'ganalytics';
 import createStore from 'unistore';
 import ossninja from '@lib/ossninja';
 import Header from './header';
@@ -9,7 +8,6 @@ import Home from '../routes/home';
 import License from '../routes/license';
 import config from '../config.json';
 
-// const ga = new GAnalytics(config.gaTrackingId, { aid: 1 });
 const model = ossninja({ config });
 const store = createStore();
 
@@ -22,17 +20,12 @@ export default function AppWrapper(props) {
 }
 
 class App extends Component {
-	handleRoute = () => {
-		if (this.ga) this.ga.send('pageview');
+	handleRoute = ({ url, previous }) => {
+		// ignore repeated routes (eg, when editing fields):
+		if (previous && url.replace(/?.*$/,'')===previous.replace(/?.*$/,'')) return;
+		// eslint-disable-next-line no-undef
+		if (typeof gtag==='function') gtag('config', config.gaTrackingId, { page_path: url });
 	};
-
-	componentDidMount() {
-		setTimeout( () => {
-			import('ganalytics').then( ({ default: GAnalytics }) => {
-				this.ga = new GAnalytics(config.gaTrackingId, { aid: 1 });
-			});
-		}, 250);
-	}
 
 	render({ url }) {
 		return (
@@ -47,6 +40,9 @@ class App extends Component {
 				{ typeof document==='undefined' && model.getAllLicensesSync && (
 					<script dangerouslySetInnerHTML={{ __html: `window.ALL_LICENSES=${JSON.stringify(model.getAllLicensesSync())}` }} />
 				) }
+
+				<script>{`dataLayer=[['js',new Date()],['config','${config.gaTrackingId}']];function gtag(){dataLayer.push(arguments)}`}</script>
+				<script async src="//www.googletagmanager.com/gtag/js?id=UA-6031694-25" />
 			</div>
 		);
 	}
